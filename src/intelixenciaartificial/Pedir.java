3ec1b7f7921d42074f437091eb549f0ac8b42562
agonzalezgonzalez <agonzalezgonzalez@victoria4.danielcastelao.org>
2017-11-09 09:31:10 +0100

    Pedir

---
+++ src/intelixenciaartificial/Pedir.java
@@ -0,0 +1,14 @@
+
+package IA;
+
+import java.util.Scanner;
+
+public class Pedir {
+    Scanner sc=new Scanner(System.in);
+    public void pedirNome(){
+    String nome;
+        System.out.println("¿Cual es tu nombre?");
+        nome=sc.next();
+    
+}
+}
