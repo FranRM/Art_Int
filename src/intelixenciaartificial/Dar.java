9ef61b143f0be57f41811517bb70b830ced5a270
femio23 <chiscazo.paco@gmail.com>
2017-11-09 09:27:11 +0100

    Creacion proxecto IA COD

---
+++ src/intelixenciaartificial/Dar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intelixenciaartificial;
+
+/**
+ *
+ * @author femio23
+ */
+public class Dar {
+   public Dar(){
+        
+    }
+    @Override
+    public String toString(){
+    return "Bos dias, cachocarne";
+    } 
+}
---
+++ src/intelixenciaartificial/IntelixenciaArtificial.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intelixenciaartificial;
+
+/**
+ *
+ * @author femio23
+ */
+public class IntelixenciaArtificial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //test
+        Dar d=new Dar();
+        System.out.println(d.toString());
+    }
+    
+}
